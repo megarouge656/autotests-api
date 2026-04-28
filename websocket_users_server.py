2636c7b2987dc270a98fcd0b0e4421b8e0753f9b
Alisher <megarouge656@gmail.com>
2026-04-28 22:57:20 +0300

    commit blue files

---
+++ websocket_users_server.py
@@ -0,0 +1,21 @@
+import asyncio
+
+import websockets
+
+
+async def echo(websocket: websockets.ServerConnection):
+    async for message in websocket:
+        print(f"Получено сообщение от пользователя: {message}")
+
+        for index in range(5):
+            await websocket.send(f"{index + 1} Сообщение пользователя: {message}", )
+
+
+async def main():
+    server = await websockets.serve(echo, "localhost", 8765)
+    print("Сервер запущен!")
+    await server.wait_closed()
+
+
+asyncio.run(main())
+
